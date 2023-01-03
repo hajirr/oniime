e1fbb617c9764d6bbb0ef88fedbf3c67bba00d4a
muhajir-artapuri <muhajir@artapuri.co.id>
2023-01-03 16:02:45 +0700

    change size

--- frontend/src/components/BannerAds.jsx
+++ frontend/src/components/BannerAds.jsx
@@ -2,7 +2,7 @@ const BannerAds = ({ url, banner }) => {
   return (
     <a href={url} target="_blank" rel="noreferrer">
       <div className=" my-4">
-        <img src={banner} alt="banner jasaviral" className="mx-auto w-full" />
+        <img src={banner} alt="banner jasaviral" className="mx-auto" />
       </div>
     </a>
   );
